349166c86f61cf08ec053123b80d37d3fd709946
voyagerdva <mariner-9@yandex.ru>
2024-02-28 20:03:51 +0300

    22 lections

--- src/main/java/nn/ru/spring/database/repository/CompanyRepository.java
+++ src/main/java/nn/ru/spring/database/repository/CompanyRepository.java
@@ -6,15 +6,17 @@ import nn.ru.spring.database.entity.Company;
 import nn.ru.spring.database.pool.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
-@Repository
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
     private final ConnectionPool pool1;
--- src/main/java/nn/ru/spring/database/repository/UserRepository.java
+++ src/main/java/nn/ru/spring/database/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package nn.ru.spring.database.repository;
 
 import nn.ru.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class UserRepository {
     private final ConnectionPool connectionPool;
 
